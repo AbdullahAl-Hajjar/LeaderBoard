20dee118e91341c8d4531dfbb93fd7b7af77c87b
AbdullahAl-Hajjar <46487867+AbdullahAl-Hajjar@users.noreply.github.com>
2019-08-04 11:55:48 -0400

    Update CreateUserTest.java

--- HtmlSampleJPA - NetBeans - Skeleton/src/java/view/CreateUserTest.java
+++ HtmlSampleJPA - NetBeans - Skeleton/src/java/view/CreateUserTest.java
@@ -16,10 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import logic.UsernameLogic;
 
-/**
- *
- * @author Gyp
- */
+
 public class CreateUserTest extends HttpServlet {
 
     /**
